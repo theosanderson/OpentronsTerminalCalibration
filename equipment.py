994749eb1b54f3dadafc0cbd38f29a8bdfcb0262
Theo Sanderson <theo@sndrsn.co.uk>
2018-06-13 22:21:45 +0100

    Update equipment.py

--- equipment.py
+++ equipment.py
@@ -3,11 +3,6 @@ from opentrons import robot, containers, instruments
 
 def getEquipment():
         equipment={}
-        ############## DECK LAYOUT BEGINS HERE
-        #CONTAINER DEFINITIONS:
-        TransposeTipBox=True
-        containers.create("epmotion30",grid=(8,1),spacing=(9.02,9.02),diameter=10,depth=55) #EpMotion Reservoir
-        containers.create("24corning",grid=(4,6),spacing=(19.304,19.304),diameter=16.26,depth=18) #24-well plate
 
         #DECK:
         equipment['trash']=containers.load('point', "C1","trash")
